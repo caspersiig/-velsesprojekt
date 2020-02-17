8f429b4e787da00c65a315d651b8804754a6421d
casper siig <47445200+caspersiig@users.noreply.github.com>
2020-02-17 13:10:01 +0100

    Den helt store
    yes

--- Klient/sketch.js
+++ Klient/sketch.js
@@ -1,12 +1,45 @@
 var socket = io.connect('http://localhost:3000');
 
-function setup(){
-createCanvas(600,600);
-background(200);
+function setup() {
+    createCanvas(200, 200);
+    background(200);
+
+    inp = createInput('');
+    inp.size(100, 20);
+    inp.position(60, 60);
+
+    registrer = createInput('');
+    registrer.size(100, 20);
+    registrer.position(60, 160);
+
+    text("Registrer", 60, 140);
+    text("Check IND", 60, 40);
+
+    socket.on("acceptered", (data) => {
+        if(data == 1){
+            alert("Checked ind");
+        }
+        if(data == 2){
+            alert("Checked ud");
+        }
+    })
 
 }
+// rejsekortnummer = 6,3,3,3,1
 
-function draw(){
 
+function keyPressed() {
+    if (keyCode === ENTER && inp.value().length == 16) {
+        data = {
+            id:inp.value(),
+            time:
+        }
 
+        socket.emit("idtilcheck", data);
+        console.log("Sendt");
+
+    } else if (keyCode === ENTER && registrer.value().length == 16) {
+        socket.emit("idtilregistering", registrer.value());
+        console.log("Sendt");
+    }
 }
--- Server.js
+++ Server.js
@@ -2,24 +2,38 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 app.use(express.static('Klient'));
-
-var socket = require('socket.io'); 
+var socket = require('socket.io');
 var io = socket(server);
-
+var fs = require('fs');
+var database = JSON.parse(fs.readFileSync('database.json', 'utf8'));
 
 io.sockets.on('connection', newConnection);
-io.sockets.on('disconnection', newDisconnect);
 
+function newConnection(socket) {
+    console.log("toget" + socket.id + "Har lige Connected");
 
-function newConnection(socket){
-    console.log(socket.id + "Har lige Connected");
-    // sockets.on('Nyspiller', () =>{
-    
-    // });
+    socket.on("idtilcheck", (data) => {
+        for (let i = 0; i < database.ider.length; i++) {
+            if (data.id == database.ider[i].id) {
+                if (database.ider[i].ercheckin == false) {
+                    socket.emit("acceptered",1);
+                    database.ider[i].ercheckin = true;
+                } else {
+                    socket.emit("acceptered",2);
+                    database.ider[i].ercheckin = false;
+                }
+            }
+        }
+    });
+    socket.on("idtilregistering", (data) => {
+        database.ider[database.ider.length] = { "id":data, "ercheckin":false };
+        fs.writeFileSync("database.json",JSON.stringify(database));
+    });
+    socket.on('disconnect', newDisconnect);
 }
 
-function newDisconnect(socket){
-console.log(socket.id + "Har lige disconnected");
+function newDisconnect(socket) {
+    console.log("toget " + socket.id + " har lige Disconnected");
+    fs.writeFileSync("database.json",JSON.stringify(database));
+
 }
-
-
